e6d1ca2e42fe1439fb7d44ea45c21a89239fc132
krutibagwe <83567614+krutibagwe@users.noreply.github.com>
2023-11-07 23:59:02 +0530

    Update mergeSort.c

--- mergeSort.c
+++ mergeSort.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1, n2 = r - m;
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
     int L[n1], R[n2];
 
-    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
-    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = arr[mid + 1 + j];
 
-    for (int i = 0, j = 0, k = l; k <= r;)
-        if (i < n1 && (j >= n2 || L[i] <= R[j])) arr[k++] = L[i++];
-        else arr[k++] = R[j++];
+    int i = 0, j = 0, k = left;
 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            ++i;
+        } else {
+            arr[k] = R[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        ++i;
+        ++k;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        ++j;
+        ++k;
+    }
 }
 
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-        merge(arr, l, m, r);
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
     }
 }
 
 void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) printf("%d ", arr[i]);
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", arr[i]);
+    }
     printf("\n");
 }
 
@@ -41,3 +70,4 @@ int main() {
 
     return 0;
 }
+
